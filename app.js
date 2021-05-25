f0ca1b42ba0d7d23d1206663ea6584d94191fb31
Patern14 <paternmalle@gmail.com>
2021-05-25 19:06:27 +0100

    It works. Need to reorder JS functions for more lisibilty.

--- app.js
+++ app.js
@@ -1,2 +1,95 @@
 console.log("App.js CONNECTED")
 
+const x_class = "x";
+const o_class = "o";
+const cells = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const endGameWindow = document.getElementById("end_game_window");
+const endGameResult = document.getElementById("end_game_result");
+const playAgainButton = document.getElementById("play_again_btn");
+
+const win_combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+let o_turn;
+
+const handleClick = (e) => {
+    console.log("Clicked")
+    const cell = e.target;
+    const currentClass = o_turn ? o_class : x_class;
+    placeMark(cell, currentClass);
+
+    if (checkWin(currentClass)) {
+        endGame(false);
+        console.log(currentClass + " win !")
+    } else if (isDraw()) {
+        endGame(true);
+    } else {
+        switchTurn()
+        setBoardClass()
+    }
+}
+
+const isDraw = () => {
+    return [...cells].every(cell => {
+        return cell.classList.contains(x_class) || cell.classList.contains(o_class);
+    })
+}
+
+const endGame = (draw) => {
+    if (draw) {
+        endGameResult.innerText = "It's a Draw!";
+    } else {endGameResult.innerText = `${o_turn ? "O's" : "X's"} Wins!`}
+
+    endGameWindow.classList.add("show")
+}
+
+const placeMark = (cell, currentClass) => {
+    cell.classList.add(currentClass)
+}
+
+const switchTurn = () => {
+    o_turn = !o_turn;
+}
+
+const setBoardClass = () => {
+    board.classList.remove(x_class);
+    board.classList.remove(o_class);
+    
+    o_turn ? board.classList.add(o_class) : board.classList.add(x_class);
+}
+
+const startGame = () => {
+    o_turn = false;
+    
+    cells.forEach(cell => {
+        cell.classList.remove(x_class);
+        cell.classList.remove(o_class);
+        //cell.removeEventListener("click", handleClick);       // TODO: why this line?
+        cell.addEventListener("click", handleClick, {once: true});
+    })
+    
+    setBoardClass();
+    endGameWindow.classList.remove("show")
+}
+startGame();
+
+playAgainButton.addEventListener("click", startGame);
+
+const checkWin = (currentClass) => {
+    return win_combinations.some(combination => {
+        return combination.every(index => {
+            return cells[index].classList.contains(currentClass)
+        })
+    })
+}
